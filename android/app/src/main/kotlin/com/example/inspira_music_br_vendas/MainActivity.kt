41d56fc6a44cc1cd938a3d5ff4b31c6889ea3e74
marllondavid <marllondaavid@gmail.com>
2025-07-10 17:54:48 -0300

    Deploy do Flutter Web

--- android/app/src/main/kotlin/com/example/inspira_music_br_vendas/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.inspira_music_br_vendas
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/src/utils/number_utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-class NumberUtils {
-  static String formatCurrency(double value) {
-    final formatter = NumberFormat.currency(locale: 'pt_BR', symbol: 'R\$');
-    return formatter.format(value);
-  }
-}
